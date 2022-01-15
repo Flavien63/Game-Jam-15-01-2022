02878e511d5a52ade02374380df449b21bdf17c4
Antoine Thirion <antoine.thirion@etu.uca.fr>
2022-01-15 18:00:42 +0100

    init classe perso

--- MainJeu/Perso.cpp
+++ MainJeu/Perso.cpp
@@ -1,1 +1,22 @@
 #include "Perso.hpp"
+
+void Perso::getPV() {
+	return PV;
+}
+
+void Perso::PVUp() {
+	PV++;
+}
+
+void Perso::PVDown() {
+	PV--;
+}
+
+void Perso::setv_saut(int val)
+{
+	v_saut = val;
+}
+
+void Perso::Sauter(Entite ent, Bloc_Jump bj) {
+	ent.deplacer()
+}
