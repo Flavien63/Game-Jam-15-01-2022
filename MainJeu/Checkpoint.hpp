ac17f3357635e92d4766e14330355f5269ff3e57
axpronnier <axel.pronnier@gmail.Com>
2022-01-16 14:06:37 +0100

    Pic define et checkpoint

--- MainJeu/Checkpoint.hpp
+++ MainJeu/Checkpoint.hpp
@@ -1,12 +1,15 @@
 #ifndef __CHECKPOINT_HPP__
 #define __CHECKPOINT_HPP__
-
 #include "Bloc.hpp"
 
-class Checkpoint :
-    public Bloc
+class Checkpoint : public Bloc
 {
-};
+	static bool actif;
 
+public:
+	Checkpoint(int largeur, int longeur, int posx, int posy);
+	static void rendreActif();
+	bool isActif();
+};
 
 #endif // !__CHECKPOINT_HPP__
